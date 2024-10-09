8c4c8ef902d6cb47c0de2c57a93c856beb0cd71f
Tokikawa_06 <106184151+Emytoky@users.noreply.github.com>
2024-10-09 08:49:41 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,7 +3,20 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function buscarInformacoes(){
     const res = await fetch(url);
     const dados = res.json();
-    console.log(dados);
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container_texto');
+
+    const pessoasConectadas = dados.total_pessoas_conectadas / 1e9;
+    const pessoasNoMundo = dados.total_pessoas_mundo / 1e9;
+    const horas = parseInt(dados.tempo_medio);
+    const minutos = Math.round((dados.tempo_medio - horas)*60);
+    const porcentagemConectada = (pessoasConectadas/pessoasNoMundo)*100;
+
+
+    paragrafo.innerHTML = 'Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente  <span>${dados.total_pessoas_conectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas}</span> horas e <span>${minutos}</span> minutos conectadas. Isso significa que aproximadamente ${porcentagemConectada}% de pessoas estão conectadas em alguma rede social.';
+
+      
+      document.getElementById('grafico-container').appendChild(paragrafo)
 }
 
 buscarInformacoes();
